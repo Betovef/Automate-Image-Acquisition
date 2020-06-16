583d035fd07198a383e7d26f98dfa5e31d8f81db
Alberto <albertovalleflores99@gmail.com>
2020-06-15 22:00:19 -0700

    Implemented Scanner reader and setted up the array to save data

--- automateSelenium.java
+++ automateSelenium.java
@@ -1,3 +1,8 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -6,15 +11,34 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class automateSelenium {
 
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws InterruptedException, FileNotFoundException {
+		
+		//set up scanner
+		Scanner scanIn = null;
+		String InputLine = "";
+		String[] addresses = new String[886];
+		String xfileLocation = "C:\\Users\\Alberto\\Desktop\\REU\\PythonProject\\Automate-Image-Acquisition\\addresses.csv";
+		
+		scanIn = new Scanner(new BufferedReader(new FileReader(xfileLocation)));
+		
+		int i = 0;
+		while(scanIn.hasNextLine()) {
+			InputLine = scanIn.nextLine();
+			addresses[i] = InputLine;
+			System.out.println(addresses[i]);
+			i++;
+		}
+		
 		System.setProperty("webdriver.chrome.driver", "C:\\webdrivers\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		String[] addresses = {"430 West 6th Street Merced","905 West 8th Street Merced","909 West 8th Street Merced","945 West 8th Street Merced","956 West 8th Street Merced"  };
+//		String[] addresses = {"430 West 6th Street Merced","905 West 8th Street Merced","909 West 8th Street Merced","945 West 8th Street Merced","956 West 8th Street Merced"  };
 		
 		driver.get("https://images.google.com/");
+//		driver.get("https://www.redfin.com/");
+//		driver.get("https://www.zillow.com/");
 		WebElement elem = null;
-		int i=0;
-		while(i<4){
+		i = 0;
+		while(i<addresses.length){
 		elem = driver.findElement(By.name("q"));
 		elem.sendKeys(addresses[i]);
 		elem.sendKeys(Keys.ENTER);
