74bc690e8b8ad33a010dc49f8a7ce5df22d3946a
Alberto <albertovalleflores99@gmail.com>
2020-06-16 21:16:13 -0700

    Implemented findElement(By.xpath) to find and print image address

--- automateSelenium.java
+++ automateSelenium.java
@@ -3,6 +3,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.File;
 import java.io.PrintWriter;
+import java.util.List;
 import java.util.Scanner;
 
 import org.openqa.selenium.By;
@@ -45,32 +46,36 @@ public class automateSelenium {
 		
 		// setting up csv file writer
 		
-		try {
-			PrintWriter pw = new PrintWriter(new File("C:\\Users\\Alberto\\Desktop\\REU\\PythonProject\\Automate-Image-Acquisition\\ImagesDatasetTest1.csv"));
-			StringBuilder sb = new StringBuilder();
-			
-//			sb.append("Address");
-//			sb.append("\r\n");
-			while(i<addresses.length) {
-				sb.append(addresses[i]);
-				sb.append("\r\n");
-				i++;
-			}
-			pw.write(sb.toString());
-			pw.close();
-			
-		} catch (Exception e) {
-			
-		}
-		
-	
+//		try {
+//			PrintWriter pw = new PrintWriter(new File("C:\\Users\\Alberto\\Desktop\\REU\\PythonProject\\Automate-Image-Acquisition\\ImagesDatasetTest1.csv"));
+//			StringBuilder sb = new StringBuilder();
+//			
+////			sb.append("Address");
+////			sb.append("\r\n");
+//			
+//			while(i<addresses.length) {
+//				sb.append(addresses[i]);
+//				sb.append("\r\n");
+//				i++;
+//			}
+//			pw.write(sb.toString());
+//			pw.close();
+//			
+//		} catch (Exception e) {
+//			
+//		}
 		
 //		 automating... 
+		String text = "";
 		while(i<addresses.length){
 		elem = driver.findElement(By.id("searchboxinput"));
 		elem.sendKeys(addresses[i]);
 		elem.sendKeys(Keys.ENTER);
-		Thread.sleep(2000);
+//		text = elem.getAttribute("src");
+//		img = driver.findElement(By.)
+		Thread.sleep(4000);
+		elem = driver.findElement(By.xpath("/html/body/jsl/div[3]/div[9]/div[8]/div/div[1]/div/div/div[1]/div[1]/button/img"));
+		System.out.println(elem.getAttribute("src"));
 		driver.findElement(By.name("q")).clear();
 		i++;
 		}
